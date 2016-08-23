796c85c926b77660d182ab40e8f7ecd4b545f0fd
conway <conwaywang@gmail.com>
2016-08-22 19:30:47 -0700

    change callback url

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
   new GitHubStrategy({
     clientID: "6bc10986fdcb003b9e85",
     clientSecret: "2ad54a7701fd5a47333c14253f5c9814fd666218",
-    callbackURL: "http://localhost:3000/api/auth/callback"
+    callbackURL: "https://queup3000.herokuapp.com/api/auth/callback"
   },
   function(accessToken, refreshToken, profile, done) {
 
