f2087d246130a3ea1ebc8ebb511233043c05ab9a
conway <conwaywang@gmail.com>
2016-08-23 13:57:28 -0700

    chnage routes for heroku post test for queues

--- server/queues/queueController.js
+++ server/queues/queueController.js
@@ -1,5 +1,5 @@
 const Queue = require('./queueModel')
-const inQueue = require('./inQueueModel')
+// const inQueue = require('./inQueueModel')
 
 module.exports = {
   enqueue,
@@ -8,31 +8,31 @@ module.exports = {
 
 function enqueue(req, res) { //signing up for a queue
   Queue.create({
-    userNumber:,
-    userID:,
-    lineName:
-    capacity:
+    userNumber: req.body.phone,
+    userID: req.body.id,
+    lineName: req.body.line,
+    capacity: req.body.capacity,
   })
-  if findall inqueue < capacity //if there are less than capacity inququed then notifiy and remove from queue
-  notify person
+  // if findall inqueue < capacity //if there are less than capacity inququed then notifiy and remove from queue
+  // notify person
   .then(() => { res.status(200).end(); })
   .catch((err) => { console.log(err); });
 }
-
-function dequeue(req, res) { //getting a text messsage for done
-  inQueue.findOne({
-    where: { lineName: req.line }
-  })
-  .then(person => {//find next person in queue for line
-    notify(person)
-  })
-  .then(() => { res.status(200).end(); })
-  .catch((err) => { console.log(err); });
-}
-
-function notify(person) { //let people know they are inqueued and remove them from queue db and insert into inqueue
-  //send message
-  //add to enquque
-  person.destroy()
+//
+// function dequeue(req, res) { //getting a text messsage for done
+//   inQueue.findOne({
+//     where: { lineName: req.line }
+//   })
+//   .then(person => {//find next person in queue for line
+//     notify(person)
+//   })
+//   .then(() => { res.status(200).end(); })
+//   .catch((err) => { console.log(err); });
+// }
+//
+// function notify(person) { //let people know they are inqueued and remove them from queue db and insert into inqueue
+//   //send message
+//   //add to enquque
+//   person.destroy()
 
 }
--- server/queues/queueRoutes.js
+++ server/queues/queueRoutes.js
@@ -2,8 +2,8 @@ queueCtrl = require('./queueController')
 
 const mountQueueRoutes = app => {
   app.route('/api/queues')
-    .get(queueCtrl.enqueue)
-    .post(queueCtrl.dequeue)
+    .post(queueCtrl.enqueue)
+    .get(queueCtrl.dequeue)
 };
 
 module.exports = mountQueueRoutes;
