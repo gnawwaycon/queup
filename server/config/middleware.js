c88493369e81a52dd015be857abe52a01dca46bb
conway <conwaywang@gmail.com>
2016-08-24 21:02:31 -0700

    intialize client

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
+const express = require('express');
+const path = require('path')
 
 const mountMiddleware = app => {
   app.use(bodyParser.urlencoded({ extended: true }))
@@ -12,6 +14,7 @@ const mountMiddleware = app => {
   app.use(session({ secret: "sompop" }));
   app.use(passport.initialize());
   app.use(passport.session());
+  app.use(express.static(path.join(__dirname, '../../client/build')))
 };
 
 module.exports = mountMiddleware;
