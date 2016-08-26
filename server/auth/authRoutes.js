fe843ac4e127c0fe1ecc858a8aa85427727fd04a
conway <conwaywang@gmail.com>
2016-08-26 15:36:58 -0700

    show req.user on loginx

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -8,23 +8,14 @@ const mountAuthRoutes = app => {
     app.get('/api/auth/callback',
     passport.authenticate('github', { failureRedirect: '/login' }),
     function(req, res) {
+      console.log('ME HERE', req.user);
       res.redirect('/');
     });
 
-    app.get('/', ensureAuthenticated, function(req, res){
-      res.render('account', { user: req.user });
+    app.get('/', function(req, res){
+      res.send(req.user)
     });
-
-    app.get('/logout', function(req, res){
-      req.logout();
-      res.logout('/');
-    });
-
-    function ensureAuthenticated(req, res, next) {
-        if (req.isAuthenticated()) { return next(); }
-    res.redirect('/api/auth')
-  }
-
+});
 }
 
 module.exports = mountAuthRoutes;
